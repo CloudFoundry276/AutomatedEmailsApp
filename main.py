9d78afe83b307f78effa7a70533262178aca14a1
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-02-03 00:43:08 +0530

    sending automated email to users of their interest with refactoring

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import datetime
 import time
 
 
+def send_mail():
+    from_date = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    to_date = datetime.datetime.now().strftime('%y-%m-%d')
+    news_feed = NewsFeed(interest=row['interest'], from_date=from_date, to_date=to_date)
+    email = yagmail.SMTP(user="pythondjango276@gmail.com", password="Hello@World123")
+    email.send(to=row['email'],
+               subject=f"Your {row['interest']} news for today!",
+               contents=f"Hi {row['name']},\nSee what's on about {row['interest']} today.\n\n{news_feed.get()}\nPython Django")
+
+
 while True:
-    if datetime.datetime.now().hour == 00 and datetime.datetime.now().minute == 19:
+    if datetime.datetime.now().hour == 00 and datetime.datetime.now().minute == 40:
         df = pandas.read_excel('people.xlsx')
         for index, row in df.iterrows():
-            from_date = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-            to_date = datetime.datetime.now().strftime('%y-%m-%d')
-            news_feed = NewsFeed(interest=row['interest'], from_date=from_date, to_date=to_date)
-            email = yagmail.SMTP(user="pythondjango276@gmail.com", password="Hello@World123")
-            email.send(to=row['email'],
-                       subject=f"Your {row['interest']} news for today!",
-                       contents=f"Hi {row['name']},\nSee what's on about {row['interest']} today.\n\n{news_feed.get()}\nPython Django")
+            send_mail()
 
     time.sleep(60)
